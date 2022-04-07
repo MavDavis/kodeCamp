7fafc1d8de92489b874abe43e3e1b480686b1a92
MavDavis <ekwesimarvelous@gmail.com>
2022-04-07 07:55:42 +0100

    new commit

--- ajax.js
+++ ajax.js
@@ -23,34 +23,47 @@ function checkForEmptyField(e){
   let input = document.getElementsByClassName('input');
   let p =document.getElementById('p-tag');
   let error = document.createTextNode('fill in your details');
-  let good = document.createTextNode('You are registered');
+  let good = document.createTextNode('You have been successfully registered');
+  p.style.display =" block"
+  let timeout =  setTimeout(()=>{p.removeChild(error)}, 8000);
+  let timeout2 =  setTimeout(()=>{p.removeChild(good)}, 8000);
+  
    for(var i = 0; i <input.length; i++){
    }
    if((input[0].value== '')){
     p.appendChild(error);
     p.style.color='red'
-    setTimeout(()=>{p.style.display='none'}, 8000);
+    timeout;
    }else if((input[1].value== '')){
     p.appendChild(error);
     p.style.color='red' ;
-    setTimeout(()=>{p.style.display='none'}, 8000); 
+    timeout;
    }else if((input[2].value== '')){
     p.appendChild(error);
     p.style.color='red'  ;
-    setTimeout(()=>{p.style.display='none'}, 8000);
+    timeout;
    }else if((input[3].value== '')){
     p.appendChild(error);
     p.style.color='red'  ;
-    setTimeout(()=>{p.style.display='none'}, 8000)
+   timeout;
    }else if((input[4].value== '')){
     p.appendChild(error);
     p.style.color='red';
-    setTimeout(()=>{p.style.display='none'}, 8000);  
+   timeout;
    }
    
    else{
     p.appendChild(good);
     p.style.color='green';
-    setTimeout(()=>{p.style.display='none'}, 8000);
+   timeout2;
    }
-}
+  //can someone check why input[i].value isnt responding.
+  if((input[0].value ='') || (input[1].value='') || (input[2].value='') || (input[3].value='') || (input[4].value='')){
+console.log('solution to keep the values and ask for details')
+}else{
+  input[0].value ='';
+   input[1].value ='';
+   input[2].value ='';
+   input[3].value ='';
+   input[4].value ='';
+}}
